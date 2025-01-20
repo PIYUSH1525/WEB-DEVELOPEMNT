18ea412647984c06dc7ffd5b084dac7ada561921
PIYUSH1525 <negipiyush125@gmail.com>
2025-01-20 22:08:55 +0530

    map function

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-
 // For Each Method
 const arr = ["amana" , "piyush" , "amit"]
 
-arr.forEach(function(val){
-    if (val == "amana")
-        console.log("present")
+// arr.forEach(function(val){
+//     if (val == "amana")
+//         console.log("present")
+// })
+const newmap = arr.map(function(value){
+    return (value + " ji")
 })
