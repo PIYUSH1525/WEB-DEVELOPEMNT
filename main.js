00a6233425c0aaa83b86aaf040fc11c392fe2552
PIYUSH1525 <negipiyush125@gmail.com>
2025-01-20 22:33:49 +0530

    filter function

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 // For Each Method
-const arr = ["amana" , "piyush" , "amit"]
+const arr = ["amana" , "piyush" , "amit","Alice","Bob","Charlie","David","Emma","Frank","Grace","Henry","Isabel","Jack","Kate","Liam","Mia","Noah","Olivia","Peter","Quinn","Rachel","Sam","Tina" , "Chandan"]
 
 // arr.forEach(function(val){
 //     if (val == "amana")
 //         console.log("present")
 // })
-const newmap = arr.map(function(value){
-    return (value + " ji")
-})
+// const newmap = arr.map(function(value){
+//     return (value + " ji")
+// })
+
+//  Filter Function 
+
+// const filterarr = arr.filter(function(val){
+//     return val.startsWith('C')
+// })
